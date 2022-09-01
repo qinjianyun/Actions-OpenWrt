48ebc5fdd915008684e79d04d942cc145625a446
qinjianyun <112633306+qinjianyun@users.noreply.github.com>
2022-09-01 22:27:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master'   feeds.conf.default
+sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main'   feeds.conf.default
+sed -i '$a src-git istore-packages https://github.com/linkease/istore-packages.git' feeds.conf.default
+sed -i '$a src-git istore https://github.com/linkease/istore.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
